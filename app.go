e133369531b14371a4de5dc49ac21a19a1953f71
chajiuqqq <chajiuqqq@gmail.com>
2023-04-28 19:32:21 +0800

    保留精确rate；改为命令行提供key；给map上锁

--- app.go
+++ app.go
@@ -1,92 +1,45 @@
 package main
 
 import (
-	"encoding/json"
-	"io/ioutil"
+	. "example/service"
+	"flag"
 	"log"
-	"net/http"
 	"strings"
 	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
-var URL = "https://rest.coinapi.io/v1/exchangerate/"
+var (
+	key            = flag.String("key", "", "access key")
+	convertService *ConvertService
+)
+const (
+	CACHE_EXPIRE = time.Second*10
+)
 
-type MyResponse struct {
-	Asset_id_base  string
-	Asset_id_quote string
-	Rate           float64
+func init() {
+	flag.Parse()
+	if key == nil || *key == "" {
+		log.Fatal("key couldn't be empty")
+		return
+	}
+	convertService = NewConvertService(*key, CACHE_EXPIRE)
 }
-
-type CacheResponse struct {
-	createTime time.Time
-	value      *MyResponse
-}
-
-type MyCache struct {
-	amount map[string]*CacheResponse
-}
-
-var mycache = &MyCache{amount: make(map[string]*CacheResponse)}
 
 func main() {
+
 	route := gin.Default()
-	// ctx,cancel := context.WithCancel(context.Background())
-
 	route.GET("/price/:currency", func(ctx *gin.Context) {
-		target := ctx.Param("currency")
-		if target == "" {
-			ctx.JSON(500, gin.H{"msg": "empty param"})
-		}
-		target = strings.ToUpper(target)
-		myres, err := getPrice(target)
+		currency := ctx.Param("currency")
+		currency = strings.ToUpper(currency)
+		res, err := convertService.Convert(currency, "USD")
 		if err != nil {
-			ctx.JSON(500, gin.H{"msg": err.Error()})
+			ctx.JSON(500, gin.H{"msg": "convert error", "error": err.Error()})
+			return
 		}
-		ctx.JSON(200, myres)
+		// convertService.PrintCache()
+		ctx.JSON(200, res)
 	})
-	go func() {
-		select {
-		// case <-ctx.Done():
-		// 	return
-		case <-time.Tick(time.Second * 10):
-			for k, v := range mycache.amount {
-				if time.Now().Sub(v.createTime) > time.Second*10 {
-					delete(mycache.amount, k)
-				}
-			}
-		}
-	}()
 	route.Run(":8080")
-}
-
-func getPrice(target string) (*MyResponse, error) {
-	if v, ok := mycache.amount[target]; ok {
-		log.Println("Use cached")
-		return v.value, nil
-	}
-	url := URL + target + "/USD"
-	client := &http.Client{}
-	r, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		return nil, err
-	}
-	r.Header.Add("X-CoinAPI-Key", "B89898B1-1DFC-4D44-AB49-4D56856A3627")
-
-	resp, _ := client.Do(r)
-	body, _ := ioutil.ReadAll(resp.Body)
-	log.Println(string(body))
-	myres := &MyResponse{}
-	err = json.Unmarshal(body, myres)
-	if err != nil {
-		return nil, err
-	}
-
-	mycache.amount[target] = &CacheResponse{
-		value:      myres,
-		createTime: time.Now(),
-	}
-	log.Println("Not Use cached")
-	return myres, nil
 }
---
+++ service/service.go
@@ -0,0 +1,132 @@
+package service
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"sync"
+	"time"
+)
+
+type QueryResponse struct {
+	Asset_id_base  string      `json:"asset_id_base"`
+	Asset_id_quote string      `json:"asset_id_quote"`
+	Rate           json.Number `json:"rate"`
+	Time           string      `json:"time"`
+}
+
+type RateBody struct {
+	createTime time.Time
+	value      *QueryResponse
+}
+
+type ConvertService struct {
+	// key: `from-to` value:Rate
+	cache   map[string]*RateBody
+	baseUrl string
+	client  *http.Client
+	header  http.Header
+	expire  time.Duration
+	rwLock  sync.RWMutex
+}
+
+func NewConvertService(key string, expire time.Duration) *ConvertService {
+	header := http.Header{}
+	header.Add("X-CoinAPI-Key", key)
+
+	service := &ConvertService{
+		cache:   map[string]*RateBody{},
+		baseUrl: "https://rest.coinapi.io/v1/exchangerate",
+		client:  &http.Client{},
+		header:  header,
+		expire:  expire,
+	}
+	ticker := time.NewTicker(time.Second)
+	go func() {
+		for {
+			select {
+			case <-ticker.C:
+				for k, v := range service.cache {
+					if time.Since(v.createTime) > service.expire {
+						delete(service.cache, k)
+					}
+				}
+			}
+		}
+
+	}()
+
+	return service
+}
+
+func (c *ConvertService) getCachedResult(from, to string) (*QueryResponse, bool) {
+	key := from + "-" + to
+	c.rwLock.RLock()
+	defer c.rwLock.RUnlock()
+	if v, ok := c.cache[key]; ok {
+		if time.Since(v.createTime) < time.Minute*10 { // 检查缓存是否过期
+			return v.value, true
+		} else {
+			delete(c.cache, key) // 如果缓存过期则删除缓存
+		}
+	}
+	return nil, false
+}
+
+func (c *ConvertService) cacheResult(from, to string, result *QueryResponse) {
+	key := from + "-" + to
+	c.rwLock.Lock()
+	defer c.rwLock.Unlock()
+	c.cache[key] = &RateBody{
+		value:      result,
+		createTime: time.Now(),
+	}
+}
+
+func (c *ConvertService) Convert(from, to string) (*QueryResponse, error) {
+	if v, ok := c.getCachedResult(from, to); ok {
+		log.Printf("From %s to %s Use cached", from, to)
+		return v, nil
+	}
+
+	url := fmt.Sprintf("%s/%s/%s", c.baseUrl, from, to)
+	r, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+	r.Header = c.header
+
+	resp, err := c.client.Do(r)
+	if err != nil {
+		return nil, err
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.StatusCode != 200 {
+		return nil, errors.New(string(body))
+	}
+
+	myres := &QueryResponse{}
+	err = json.Unmarshal(body, myres)
+	if err != nil {
+		return nil, err
+	}
+
+	c.cacheResult(from, to, myres)
+	log.Printf("From %s to %s Not Use cached", from, to)
+	return myres, nil
+}
+
+func (c *ConvertService) PrintCache() {
+	for k, v := range c.cache {
+		expire := c.expire.Seconds() - time.Since(v.createTime).Seconds()
+		log.Println(k, v.value.Rate, expire, "s")
+	}
+}
